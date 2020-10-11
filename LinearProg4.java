514dc353be5a37bcf10af3bee636f1376908ace8
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-11 13:06:38 +0300

    линейная программа 1

---
+++ LinearProg4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LinearProg4 {
+    public static void main(String[] args) {
+
+        System.out.print("Введите значение R  в виде nnn,ddd: ");
+        Scanner scan = new Scanner(System.in);
+        double r = scan.nextDouble();
+
+        double result = (r * 1000)%1000 + (int) r/1000.0;
+        System.out.println("В обратном порядке= " + result);
+
+    }
+}
+
