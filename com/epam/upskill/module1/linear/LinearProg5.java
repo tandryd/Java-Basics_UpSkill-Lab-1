1382dd5ca2b3f7044200f1ec21c443fc053cc2c0
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 16:40:01 +0300

    Изменено имя переменной t>>> tineInSecond

--- com/epam/upskill/module1/linear/LinearProg5.java
+++ com/epam/upskill/module1/linear/LinearProg5.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class LinearProg5 {
     public static void main(String[] args) {
         System.out.print("Введите значение секунд: ");
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int  timeInSecond = scanner.nextInt();
 
-        int HH = t / 3600;
-        int MM = (t - HH*3600)/60; // (t/60)%60
-        int SS = t - (HH * 3600) - (MM * 60); // T%60
-        System.out.println(t + "c = " + HH + "ч " + MM + "мин " + SS + " сек");
+        int hours = timeInSecond / 3600;
+        int minutes = (timeInSecond - hours*3600)/60; // (t/60)%60
+        int seconds = timeInSecond - (hours * 3600) - (minutes * 60); // T%60
+        System.out.println(timeInSecond + "c = " + hours + "ч " + minutes + "мин " + seconds + " сек");
     }
     }
