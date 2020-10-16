47d0572249543db0cf73880f52b3665cb88fd1db
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 13:59:38 +0300

    MathUtil named

--- com/epam/upskill/module1/linear/MathUtils.java
+++ com/epam/upskill/module1/linear/MathUtils.java
@@ -1,6 +1,6 @@
 package com.epam.upskill.module1.linear;
 
-     class Calculator { //методы подсчета в отдельном классе
+     class MathUtils { //методы подсчета в отдельном классе
        double linearProg1Util(double a, double b, double c)
        {
            return ((a - 3) * b / 2) + c;
