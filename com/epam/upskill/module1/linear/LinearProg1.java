ed1432b6807113b540526dd037a662bfdff25486
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 13:56:35 +0300

    реализован вызов метода подсчета из отдельного класса количество цифр после запятой в результате снижено до 5

--- com/epam/upskill/module1/linear/LinearProg1.java
+++ com/epam/upskill/module1/linear/LinearProg1.java
@@ -3,19 +3,21 @@ package com.epam.upskill.module1.linear;
 import java.util.*;
 
 public class LinearProg1 {
+
     public static void main(String[] args) {
 
-        System.out.println("Введите значение a:");
-        Scanner scan = new Scanner(System.in);
-        double a = scan.nextDouble();
+        System.out.print("Введите значение a:");
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
 
-        System.out.println("Введите значение b:");
-        double b = scan.nextDouble();
+        System.out.print("Введите значение b:");
+        double b = scanner.nextDouble();
 
-        System.out.println("Введите значение c:");
-        double c = scan.nextDouble();
+        System.out.print("Введите значение c:");
+        double c = scanner.nextDouble();
 
-        double z = ((a - 3) * b / 2) + c;
-        System.out.printf("значение  функции z = ( (a – 3 ) * b / 2) + c  = %.10f", b, z);
+        MathUtils functionZ = new MathUtils();
+        System.out.printf("значение  функции z = ( (a – 3 ) * b / 2) + c  = %.5f", functionZ.linearProg1Util(a, b, c) ); //вызов метода посчета из класса MathUtils
+
     }
 }
