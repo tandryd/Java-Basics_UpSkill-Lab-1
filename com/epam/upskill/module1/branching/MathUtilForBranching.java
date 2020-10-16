6a9ac7336065fbc8c61b636a47126cc8c673a600
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 17:34:27 +0300

    introduced findMin/findMax methods

--- com/epam/upskill/module1/branching/MathUtilForBranching.java
+++ com/epam/upskill/module1/branching/MathUtilForBranching.java
@@ -1,2 +1,11 @@
-package com.epam.upskill.module1.branching;public class MathUtilForBranching {
+package com.epam.upskill.module1.branching;
+
+public class MathUtilsForBranching {
+    double findMin(double a, double b) {
+        return a < b ? a : b;
+    }
+    double findMax (double a, double b) {
+        return a > b ? a : b;
+    }
+
 }
