bae1da11c066057d26d8e902c432a6d8c990465b
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 16:24:59 +0300

    camelCase/ if {}

--- com/epam/upskill/module1/branching/BranchingProg3.java
+++ com/epam/upskill/module1/branching/BranchingProg3.java
@@ -7,25 +7,26 @@ public class BranchingProg3 {
 
         System.out.print("Введите координату x точки 1. x1 = ");
         Scanner scan = new Scanner(System.in);
-        int point1_x = scan.nextInt();
+        int xOfpoint1 = scan.nextInt();
 
         System.out.print("Введите координату y точки 1. y1 = ");
-        int point1_y = scan.nextInt();
+        int yOfpoint1 = scan.nextInt();
 
         System.out.print("Введите координату x точки 2. x2 = ");
-        int point2_x = scan.nextInt();
+        int xOfpoint2 = scan.nextInt();
 
         System.out.print("Введите координату y точки 2. y2 = ");
-        int point2_y = scan.nextInt();
+        int yOfpoint2 = scan.nextInt();
 
         System.out.print("Введите координату x точки 3. x3 = ");
-        int point3_x = scan.nextInt();
+        int xOfpoint3 = scan.nextInt();
 
         System.out.print("Введите координату y точки 3. y1 = \" ");
-        int point3_y = scan.nextInt();
+        int yOfpoint3 = scan.nextInt();
 
-        if ((point3_x - point1_x) / (point2_x - point1_x) == (point3_y - point1_y) / (point2_y - point1_y))
+        if ((xOfpoint3 - xOfpoint1) / (xOfpoint2 - xOfpoint1) == (yOfpoint3 - yOfpoint1) / (yOfpoint2 - yOfpoint1)) {
             System.out.println("Точки с заданными координатами лежат на одной прямой");
+        }
 
         else System.out.println("Точки с заданными координатами не лежат на одной прямой");
 
