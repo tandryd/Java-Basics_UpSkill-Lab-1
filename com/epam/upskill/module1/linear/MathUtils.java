c7b312dab166f7199d171b5225f827b070e35881
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 13:54:49 +0300

    реализован вызов метода подсчета из отдельного класса MathUtil количество цифр после запятой в результате уменьшено до 5

---
+++ com/epam/upskill/module1/linear/MathUtils.java
@@ -0,0 +1,19 @@
+package com.epam.upskill.module1.linear;
+
+     class Calculator { //методы подсчета в отдельном классе
+       double linearProg1Util(double a, double b, double c)
+       {
+           return ((a - 3) * b / 2) + c;
+       }
+
+
+
+
+
+
+
+
+
+
+
+}
