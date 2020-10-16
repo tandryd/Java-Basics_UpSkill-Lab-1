9a4c17734b916d8808c5fc87676c35a0a109d41d
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 16:22:04 +0300

    findMin/findMax methods

--- com/epam/upskill/module1/branching/BranchingProg2.java
+++ com/epam/upskill/module1/branching/BranchingProg2.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class BranchingProg2 {
     public static void main(String[] args) {
-        double minAB, minBC, maxResult;
 
         System.out.print("Введите значениие a: ");
         Scanner scan = new Scanner(System.in);
@@ -19,17 +18,11 @@ public class BranchingProg2 {
         System.out.print("Введите значениие d: ");
         double d = scan.nextDouble();
 
-        if (a>b)
-            minAB=b;
-            else minAB=a;
-        if (c>d)
-            minBC=d;
-            else minBC=c;
+        MathUtilsForBranching minAandB= new MathUtilsForBranching();
+        MathUtilsForBranching minCandD = new MathUtilsForBranching();
+        MathUtilsForBranching maxOfTwoMin = new MathUtilsForBranching();
 
-        if (minAB > minBC)
-            maxResult = minAB;
-        else maxResult = minBC;
-        System.out.print("значение max = " + maxResult);
+        System.out.print("значение max = " + maxOfTwoMin.findMax ((minAandB.findMin (a,b)), (minCandD.findMin(c,d))));
 
     }
 }
