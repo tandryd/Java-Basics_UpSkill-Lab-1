0e3a6a8f3ccb2854b202d3e6f095ba94fcd09dd3
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 16:18:58 +0300

    camelCase/ if {}

--- com/epam/upskill/module1/branching/BranchingProg1.java
+++ com/epam/upskill/module1/branching/BranchingProg1.java
@@ -6,15 +6,16 @@ public class BranchingProg1 {
     public static void main(String[] args) {
         System.out.println("Введите значениие угла х: ");
         Scanner scan = new Scanner(System.in);
-        int angle_X = scan.nextInt();
+        int angleX = scan.nextInt();
 
         System.out.println("Введите значениие угла angle_Y: ");
-             int angle_Y = scan.nextInt();
+             int angleY = scan.nextInt();
 
-        if (angle_X + angle_Y <180) {
+        if (angleX + angleY < 180) {
             System.out.println("Треугольник с такими углами существует. ");
-            if (angle_X + angle_Y ==90)
+            if (angleX + angleY == 90) {
                 System.out.print("Он прямоугольный.");
+            }
             else System.out.print("Он не прямоугольный.");
         }
         else
