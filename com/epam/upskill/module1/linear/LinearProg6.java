bb7e66c1d2582bffdce5c1f2a194b3cd8f3b4411
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 16:37:35 +0300

    Изменено наименование объекта scan>>scanner

--- com/epam/upskill/module1/linear/LinearProg6.java
+++ com/epam/upskill/module1/linear/LinearProg6.java
@@ -7,11 +7,11 @@ public class LinearProg6 {
     public static void main(String[] args) {
 
         System.out.println("Введите значениие х: ");
-        Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
 
         System.out.println("Введите значение y: ");
-        int y = scan.nextInt();
+        int y = scanner.nextInt();
 
        Boolean z = (((y >= 0 && y <= 4 && abs(x) <= 2) || (y <= 0 && y >= -3 && abs(x) <= 4)) && (x != 0 || y != -1));
 
