5ee9247a8ca2ad022802e3c66208af0301384242
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-16 16:48:55 +0300

    Изменено мя переменной number_1 >> number1

--- com/epam/upskill/module1/cycles/CyclesProg8.java
+++ com/epam/upskill/module1/cycles/CyclesProg8.java
@@ -1,24 +1,25 @@
 package com.epam.upskill.module1.cycles;
 
+
 import java.util.*;
 public class CyclesProg8 {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int number_1, number_2;
+        int number1, number2;
         System.out.print("Введите первое число: ");
-        number_1 = scanner.nextInt();
+        number1 = scanner.nextInt();
         ArrayList<Integer> array1 = new ArrayList<Integer>();
             do {
-                array1.add(number_1 % 10);
-                number_1 /= 10;
-            } while (number_1 > 0);
+                array1.add(number1 % 10);
+                number1 /= 10;
+            } while (number1 > 0);
         System.out.print("Введите второе число: ");
-        number_2 = scanner.nextInt();
+        number2 = scanner.nextInt();
         ArrayList<Integer> array2 = new ArrayList<Integer>();
             do {
-                array2.add(number_2 % 10);
-                number_2 /= 10;
-            } while (number_2 > 0);
+                array2.add(number2 % 10);
+                number2 /= 10;
+            } while (number2 > 0);
         array1.retainAll(array2);
         System.out.println("Cовпадают цифры: " + array1);
     }
