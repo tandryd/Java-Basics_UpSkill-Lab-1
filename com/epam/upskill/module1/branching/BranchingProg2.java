275dba7a82509608915f2dfd0f2ea7805c027417
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-24 13:16:25 +0300

    Change method import

--- com/epam/upskill/module1/branching/BranchingProg2.java
+++ com/epam/upskill/module1/branching/BranchingProg2.java
@@ -1,28 +1,19 @@
 package com.epam.upskill.module1.branching;
-
+import static  com.epam.upskill.module1.branching.MathUtilsForBranching.*;
 import java.util.Scanner;
 
 public class BranchingProg2 {
     public static void main(String[] args) {
-
         System.out.print("Введите значениие a: ");
         Scanner scan = new Scanner(System.in);
         double a = scan.nextDouble();
-
         System.out.print("Введите значениие b: ");
         double b = scan.nextDouble();
-
         System.out.print("Введите значениие c: ");
         double c = scan.nextDouble();
-
         System.out.print("Введите значениие d: ");
         double d = scan.nextDouble();
-
-        MathUtilsForBranching minAandB = new MathUtilsForBranching();
-        MathUtilsForBranching minCandD = new MathUtilsForBranching();
-        MathUtilsForBranching maxOfTwoMin = new MathUtilsForBranching();
-
-        System.out.print("значение max = " + maxOfTwoMin.findMax ((minAandB.findMin (a,b)), (minCandD.findMin(c,d))));
+        System.out.print("значение max = " + findMax(findMin(a,b), findMin(c,d)));
 
     }
 }
--- com/epam/upskill/module1/linear/LinearProg1.java
+++ com/epam/upskill/module1/linear/LinearProg1.java
@@ -1,7 +1,5 @@
 package com.epam.upskill.module1.linear;
-
 import java.util.*;
-
 public class LinearProg1 {
 
     public static void main(String[] args) {
@@ -16,8 +14,6 @@ public class LinearProg1 {
         System.out.print("Введите значение c:");
         double c = scanner.nextDouble();
 
-        MathUtils functionZ = new MathUtils();
-        System.out.printf("значение  функции z = ( (a – 3 ) * b / 2) + c  = %.5f", functionZ.linearProg1Util(a, b, c) ); //вызов метода посчета из класса MathUtils
-
+        System.out.printf("значение  функции z = ( (a – 3 ) * b / 2) + c  = %.5f", MathUtils.linearProg1Util(a, b, c) );
     }
 }
