74b2d1e14a40b02f09b56357be21bc4e93182e8b
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-10-27 13:41:40 +0300

    Module 2/ Sorting/ SortingUtils

--- com/epam/upskill/module2/sorting/SortingUtils.java
+++ com/epam/upskill/module2/sorting/SortingUtils.java
@@ -1,2 +1,39 @@
-package com.epam.upskill.module2.sorting;public class SortingUtils {
+package com.epam.upskill.module2.sorting;
+import java.util.Scanner;
+class SortingUtils {
+    static int getMyArrayLength(Scanner scanner) {
+        int myArrayLength;
+        while (true) {
+            try {
+                System.out.print("Enter the length of the array: ");
+                myArrayLength = Integer.parseInt(scanner.nextLine());
+                if (myArrayLength < 0) {
+                    throw new IllegalArgumentException();
+                }
+            } catch (NegativeArraySizeException | NumberFormatException e) {
+                System.out.println("Incorrect data. Please enter a natural number ");
+                continue;
+            }
+            break;
+        }
+        return myArrayLength;
+    }
+
+    static int [] getArrayIntFromKeyboard (int arrayLength){
+        Scanner scanner = new Scanner(System.in);
+        int [] myArrayInt = new int[arrayLength];
+        System.out.println("Insert array elements:");
+        for (int i = 0; i < myArrayInt.length; i++) {
+            myArrayInt [i] = scanner.nextInt();
+        }
+        return myArrayInt;
+    }
+
+    static int[] getArrayRandom (int arrayLength){
+        int[] myArray = new int [arrayLength];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = (int) (1 + Math.random() * myArray.length);
+        }
+        return myArray;
+    }
 }
