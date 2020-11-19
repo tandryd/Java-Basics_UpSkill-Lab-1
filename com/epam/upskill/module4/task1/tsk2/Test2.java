d13e81e2cfa5854c16723c3faf1a1d2b62b39a79
Andrei Taneyeu <andrei_taneyeu@epam.com>
2020-11-19 18:59:29 +0300

    Module 4. Task1. Test2

---
+++ com/epam/upskill/module4/task1/tsk2/Test2.java
@@ -0,0 +1,37 @@
+/*
+2. Создйте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами.
+Добавьте конструктор, инициализирующий члены класса по умолчанию.
+Добавьте set- и get- методы для полей экземпляра класса.
+ */
+package com.epam.upskill.module4.task1.tsk2;
+
+public class Test2 {
+    int a;
+    int b;
+
+    public Test2 (int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Test2() {
+        this.a = 1;
+        this.b = 2;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
